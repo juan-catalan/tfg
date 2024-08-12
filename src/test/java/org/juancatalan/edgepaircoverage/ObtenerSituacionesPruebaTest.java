3d446c7715cca72ae89b0a80199c571e9ab3f011
juan-catalan <juancatalan2001@gmail.com>
2024-08-12 10:54:43 +0200

    Refactor: crear NumSituacionsPruebaVerifier

--- src/test/java/org/juancatalan/edgepaircoverage/ObtenerSituacionesPruebaTest.java
+++ src/test/java/org/juancatalan/edgepaircoverage/ObtenerSituacionesPruebaTest.java
@@ -3,6 +3,7 @@ package org.juancatalan.edgepaircoverage;
 import org.jgrapht.graph.DirectedPseudograph;
 import org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1415.Convocatoria2;
 import org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1819.Convocatoria1;
+import org.juancatalan.edgepaircoverage.verifiers.NumSituacionsPruebaVerifier;
 import org.junit.jupiter.api.Test;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.AbstractInsnNode;
@@ -18,73 +19,45 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.objectweb.asm.Opcodes.ASM4;
 
 class ObtenerSituacionesPruebaTest {
-
-    void verificarNumSituacionesPrueba(Class clase, String nombreMetodo, int numSituacionesPrueba){
-        String idMetodo = clase.getName().concat(".").concat(nombreMetodo);
-
-        ClassNode cn = new ClassNode(ASM4);
-        ClassReader cr;
-        try {
-            cr = new ClassReader(clase.getName());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        cr.accept(cn, 0);
-        Optional<MethodNode> metodo = cn.methods.stream().filter(m -> m.name.equals(nombreMetodo)).findFirst();
-
-        assertFalse(metodo.isEmpty());
-
-
-        InsnList listaInstrucciones = metodo.get().instructions;
-        AddPrintConditionsTransformer transformer = AddPrintConditionsTransformer.getInstance();
-        transformer.addNodoToIntger(idMetodo);
-        transformer.addNodoLinenumber(idMetodo);
-        DirectedPseudograph<AbstractInsnNode, BooleanEdge> controlFlowGraph = transformer.getControlFlowAnalyser().getControlFlowGraph(idMetodo, listaInstrucciones);
-        Set<EdgePair> situacionesPrueba = transformer.obtenerSituacionesPrueba(idMetodo, controlFlowGraph);
-
-
-        assertEquals(numSituacionesPrueba, situacionesPrueba.size());
-    }
-
     @Test
     void testCurso1415Convocatoria2() {
-        verificarNumSituacionesPrueba(Convocatoria2.class,
+        NumSituacionsPruebaVerifier.verify(Convocatoria2.class,
                 "imagen", 10);
     }
 
     @Test
     void testCurso1516Convocatoria1() {
-        verificarNumSituacionesPrueba(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1516.Convocatoria1.class,
+        NumSituacionsPruebaVerifier.verify(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1516.Convocatoria1.class,
                 "esPrimo", 12);
     }
 
     @Test
     void testCurso1617Convocatoria1() {
-        verificarNumSituacionesPrueba(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1617.Convocatoria1.class,
+        NumSituacionsPruebaVerifier.verify(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1617.Convocatoria1.class,
                 "buscar", 10);
     }
 
     @Test
     void testCurso1819Convocatoria1() {
-        verificarNumSituacionesPrueba(Convocatoria1.class,
+        NumSituacionsPruebaVerifier.verify(Convocatoria1.class,
                 "buscar", 12);
     }
 
     @Test
     void testCurso1920Convocatoria1() {
-        verificarNumSituacionesPrueba(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1920.Convocatoria1.class,
+        NumSituacionsPruebaVerifier.verify(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso1920.Convocatoria1.class,
                 "minimo", 11);
     }
 
     @Test
     void testCurso2122Convocatoria1() {
-        verificarNumSituacionesPrueba(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso2122.Convocatoria1.class,
+        NumSituacionsPruebaVerifier.verify(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso2122.Convocatoria1.class,
                 "replaceDigits", 12);
     }
 
     @Test
     void testCurso2223Convocatoria1() {
-        verificarNumSituacionesPrueba(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso2223.Convocatoria1.class,
+        NumSituacionsPruebaVerifier.verify(org.juancatalan.edgepaircoverage.ejerciciosExamen.curso2223.Convocatoria1.class,
                 "calcularPuntuacion", 12);
     }
 }
---
+++ src/test/java/org/juancatalan/edgepaircoverage/verifiers/NumSituacionsPruebaVerifier.java
@@ -0,0 +1,48 @@
+package org.juancatalan.edgepaircoverage.verifiers;
+
+import org.jgrapht.graph.DirectedPseudograph;
+import org.juancatalan.edgepaircoverage.AddPrintConditionsTransformer;
+import org.juancatalan.edgepaircoverage.BooleanEdge;
+import org.juancatalan.edgepaircoverage.EdgePair;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodNode;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.objectweb.asm.Opcodes.ASM4;
+
+public class NumSituacionsPruebaVerifier {
+    public static void verify(Class clase, String nombreMetodo, int numSituacionesPrueba){
+        String idMetodo = clase.getName().concat(".").concat(nombreMetodo);
+
+        ClassNode cn = new ClassNode(ASM4);
+        ClassReader cr;
+        try {
+            cr = new ClassReader(clase.getName());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        cr.accept(cn, 0);
+        Optional<MethodNode> metodo = cn.methods.stream().filter(m -> m.name.equals(nombreMetodo)).findFirst();
+
+        assertFalse(metodo.isEmpty());
+
+
+        InsnList listaInstrucciones = metodo.get().instructions;
+        AddPrintConditionsTransformer transformer = AddPrintConditionsTransformer.getInstance();
+        transformer.addNodoToIntger(idMetodo);
+        transformer.addNodoLinenumber(idMetodo);
+        DirectedPseudograph<AbstractInsnNode, BooleanEdge> controlFlowGraph = transformer.getControlFlowAnalyser().getControlFlowGraph(idMetodo, listaInstrucciones);
+        Set<EdgePair> situacionesPrueba = transformer.obtenerSituacionesPrueba(idMetodo, controlFlowGraph);
+
+
+        assertEquals(numSituacionesPrueba, situacionesPrueba.size());
+    }
+}
